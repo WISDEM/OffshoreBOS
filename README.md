8a5068fcee5e79785c50265d4dbb6d8c1fe801bf
gbarter <garrett.barter@nrel.gov>
2019-04-03 08:45:42 -0600

    updating readme and setup and changelogs for openmdao1 release

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # OffshoreBOS
+
+An offshore balance of station (BOS) model for estimating the costs in assembly, installation, financing, and decommissioning of offshore wind plants.
+
+Author: [NREL WISDEM Team](mailto:systems.engineering@nrel.gov) 
+
+## Documentation
+
+See local documentation in the `docs`-directory or access the online version at <http://wisdem.github.io/OffshoreBOS/>
+
+## Prerequisites
+
+OffshoreBOS requires a C++ compiler
+
+## Installation
+
+For detailed installation instructions of WISDEM modules see <https://github.com/WISDEM/WISDEM> or to install OffshoreBOS by itself do:
+
+    $ python setup.py install
+
+For software issues please use <https://github.com/WISDEM/OffshoreBOS/issues>.  For functionality and theory related questions and comments please use the NWTC forum for [Systems Engineering Software Questions](https://wind.nrel.gov/forum/wind/viewtopic.php?f=34&t=1002).
+
--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ import os, platform
 
 
 if platform.system() == 'Windows':
-    # Note: must use mingw compiler on windows or a Visual C++ compiler version that supports std=c++11
     arglist = ['-std=gnu++11','-fPIC']
 else:
     arglist = ['-std=c++11','-fPIC']
@@ -16,14 +15,12 @@ setup(
     name='OffshoreBOS',
     version='1.0',
     description='Offshore balance of station model',
-    author='Garrett E. Barter',
-    author_email='garrett.barter@nrel.gov',
+    author='NREL WISDEM Team',
+    author_email='systems.engineering@nrel.gov',
     package_dir={'': 'src'},
     py_modules=['offshorebos'],
     package_data={'offshorebos': []},
     packages=['offshorebos'],
-    #install_requires=['airfoilprep.py>=0.1'],  # , 'zope.interface'],
-    # test_suite='test.test_ccblade.py',
     license='Apache License, Version 2.0',
     ext_modules=[Extension('lib_wind_obos', ['src/offshorebos/lib_wind_obos.cpp',
                                              'src/offshorebos/lib_wind_obos_cable_vessel.cpp',
